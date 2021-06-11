df1b5e1e9168ccaf16535babd820cdb7c6325512
rajkishorepatra <rpatrasm@gmail.com>
2021-06-11 19:22:25 +0530

    note file changed

--- notes.js
+++ notes.js
@@ -1,7 +1,19 @@
+var list = document.getElementById("messages");
+
 function myfunction() {
-    var newMessage = document.createElement("li");
-    var list = document.getElementById("messages");
-    newMessage.innerHTML= document.getElementById("task").value;
+	var text = document.getElementById("textarea");
+    var title = document.getElementById("noteTitle");
+    var newMessage = document.createElement("div");
+    newMessage.classList.add("text-dark");
+    newMessage.classList.add("col-md-3");
+    newMessage.classList.add("col-sm-12");
+    newMessage.classList.add("col-sm-12");
+    newMessage.innerHTML = '<div class="card mt-3">' +
+    						    '<img class="card-img-top" src="https://source.unsplash.com/400x300/?'+ title.value + '" alt="Card image cap">' +
+  									'<div class="card-body">' +
+    									'<h5 class="card-title">'+ title.value +'</h5>' +
+     										'<p class="card-text custom-card">'+ text.value +'</p>'+
+    								'</div>' + 
+  							'</div>';
     list.appendChild(newMessage);
-    task.value="";
 }
